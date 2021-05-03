9b2d8eaf68cc616a256907aaacf89d37b57ed088
ujjwal <ujjwalkumar3067@gmail.com>
2021-05-03 15:11:06 -0400

    adding password hash logic

--- src/app/models.py
+++ src/app/models.py
@@ -2,6 +2,7 @@
 
 from app import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # user inherits db.Model class
 class User(db.Model):
@@ -11,8 +12,18 @@ class User(db.Model):
     password_hash   = db.Column(db.String(128))
     posts           = db.relationship("Post", backref = "author", lazy = "dynamic")
 
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    # set's a new password hash for the user
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    # check if the password inputed  is correct ( compares with password_hash )
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
 
 class Post(db.Model):
     id              = db.Column(db.Integer, primary_key = True)
--- src/microblog.py
+++ src/microblog.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 
-from app import app
+from app import app , db
+from app.models import User , Post
+
+# config for flask shell commands
+@app.shell_context_processor
+def  make_shell_context():
+    return { 'db' : db,  "User" : User ,  "Post" : Post }
