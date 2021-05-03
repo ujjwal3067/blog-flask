ea1463a7e6e56f7384ea8aa26b288d3de4eb4927
ujjwal <ujjwalkumar3067@gmail.com>
2021-05-03 10:50:58 -0400

    added post database model

--- src/app/models.py
+++ src/app/models.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
 from app import db
+from datetime import datetime
 
 # user inherits db.Model class
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id              = db.Column(db.Integer, primary_key = True)
     username        = db.Column(db.String(64), index = True, unique = True)
     email           = db.Column(db.String(120), index = True, unique = True)
     password_hash   = db.Column(db.String(128))
+    posts           = db.relationsships("Post", backref = "author", lazy = "dynamic")
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class Post(db.Model):
+    id              = db.Column(db.Integer, primary_key = True)
+    body            = db.Column(db.String(140))
+    timestamp       = db.Column(db.DateTime, index = True, default=datetime.utcnow)
+    user_id         = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Post  {}>'.format(self.body)
